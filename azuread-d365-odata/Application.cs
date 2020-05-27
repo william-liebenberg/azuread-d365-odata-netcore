e24d7d3974a5fa1e450ea92d578aa515837a1b91
William Liebenberg <awliebenberg@outlook.com>
2020-05-27 23:41:47 +1000

    add simple hello world app with Config, DI and Logging

---
+++ azuread-d365-odata/Application.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace D365_AzureAD_ODataQuery
+{
+	public class Application
+	{
+		private readonly ILogger<Application> _logger;
+
+		private readonly string _azureTenantId;
+		private readonly string _applicationId;
+		private readonly string _applicationSecret;
+		private readonly string _scope;
+		private readonly string _tokenUrl;
+
+		public Application(ILogger<Application> logger, IConfiguration config)
+		{
+			_logger = logger;
+
+			_applicationId = config["ApplicationId"];
+			_applicationSecret = config["ApplicationSecret"];
+
+			_azureTenantId = config["AzureTenantId"];
+			_tokenUrl = $"https://login.microsoftonline.com/{_azureTenantId}/oauth2/v2.0/token";
+			
+			string crmUrl = config["CrmUrl"];
+			string scope = config["Scope"];
+			_scope = $"https://{crmUrl}/{scope}";
+		}
+
+		public async Task<bool> Run(string[] args)
+		{
+			_logger.LogInformation("Hello world!");
+			_logger.LogInformation("ApplicationId: " + _applicationId);
+			_logger.LogInformation("TokenUrl: " + _tokenUrl);
+			_logger.LogInformation("Scope: " + _scope);
+			return await Task.FromResult(true);
+		}
+	}
+}
---
+++ azuread-d365-odata/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace D365_AzureAD_ODataQuery
+{
+	internal class Program
+	{
+		public static async Task<int> Main(string[] args)
+		{
+			var services = new ServiceCollection();
+			ConfigureServices(services, args);
+
+			try
+			{
+				await using ServiceProvider sp = services.BuildServiceProvider();
+				var app = sp.GetService<Application>();
+				return await app.Run(args) ? 0 : 1;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("ERROR: " + e);
+			}
+
+			return -1;
+		}
+
+		private static void ConfigureServices(IServiceCollection services, string[] args)
+		{
+			IConfigurationRoot config = AddConfiguration(services, args);
+			ConfigureLogging(services, config);
+			services.AddSingleton<Application>();
+
+			// Add other services
+		}
+
+		private static void ConfigureLogging(IServiceCollection services, IConfigurationRoot config)
+		{
+			services.AddLogging(c => c.AddSerilog());
+			ConfigureSerilog(config);
+		}
+
+		private static void ConfigureSerilog(IConfiguration config)
+		{
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Debug()
+				.ReadFrom.Configuration(config)
+				.CreateLogger();
+
+			Log.Logger.Information("Application starting...");
+		}
+
+		private static IConfigurationRoot AddConfiguration(IServiceCollection services, string[] args)
+		{
+			IConfigurationBuilder configBuilder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: true)
+				.AddJsonFile(path: "appsettings.local.json", optional: true, reloadOnChange: true);
+
+			configBuilder
+				.AddEnvironmentVariables()
+				.AddCommandLine(args);
+
+			IConfigurationRoot config = configBuilder.Build();
+			services.AddSingleton<IConfiguration>(config);
+			return config;
+		}
+	}
+}
